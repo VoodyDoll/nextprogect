d524c5af5423036e773716f0532dad399946362d
Stas <atlantisscale@gmail.com>
2024-11-23 21:19:24 +0300

    работа getstaticpaths

--- pages/rump/[id].js
+++ pages/rump/[id].js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Post = ({ post }) => (
-  <div>
-    <h1>{post}</h1>
-  </div>
-)
+// const Post = ({ post }) => (
+//   <div>
+//     <h1>{post}</h1>
+//   </div>
+// )
 
-export default Post
+// export default Post
 
 export async function getStaticPaths() {
   return {
@@ -19,12 +19,12 @@ export async function getStaticPaths() {
   };
 }
 
-export function getStaticProps({params}){
-  let id = params.id
-  return {
-        props: { post: id}
-      }
-}
+// export function getStaticProps({params}){
+//   let id = params.id
+//   return {
+//         props: { post: id}
+//       }
+// }
 
 // export let getStaticPath = async () => {
 //   let resp = await fetch(`http://localhost:5000/2`)
@@ -43,38 +43,38 @@ export function getStaticProps({params}){
 // export async function getStaticProps(params) {
   
 
-// // export let getStaticProps = async ({params}) => {
-// //   let id = context.params.id
+export let getStaticProps = async ({params}) => {
+  let id = params.id
 
-//   let resp = await fetch(`http://localhost:5000/rump/${params.id}`)
-//   let data = await resp.json()
-//   console.log(data)
-//   return {
-//     props: { products: {data}}
-//   }
-// // }
-// }
-// const Details = ({ products }) => {
+  let resp = await fetch(`http://localhost:5000/rump/${id}`)
+  let data = await resp.json()
+  console.log(data)
+  return {
+    props: { products: data}
+  }
 
-//   return (
+}
+const Details = ( {products} ) => {
 
-//     <div>Details:
-//       {products.map((item) => {
-//         return (
-//           <center>
+  return (
 
-//             <div>
-//               <Image src={`/img/${item.img}`} width={350} height={250} alt='not foto' objectFit='cover' ></Image>
-//             </div>
-//             <p key={item.id} style={{ color: 'red' }}>{item.title} : {item.prise} руб</p>
-//             <button>Add bag</button>
-//           </center>
-//         )
-//       })}
+    <div>Details:
+      {products.map((item) => {
+        return (
+          <center>
+
+            <div>
+              <Image src={`/img/${item.img}`} width={350} height={250} alt='not foto' objectFit='cover' ></Image>
+            </div>
+            <p key={item.id} style={{ color: 'red' }}>{item.title} : {item.prise} руб</p>
+            <button>Add bag</button>
+          </center>
+        )
+      })}
 
 
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default Details
+export default Details
